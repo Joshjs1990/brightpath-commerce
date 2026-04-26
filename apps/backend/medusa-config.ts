dfea49853793b082be4329d58a9a94c1c840dcf9
Joshjs1990 <joshstewart90@gmail.com>
2026-04-26 12:59:34 +0200

    Add production admin config

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -3,14 +3,19 @@ import { loadEnv, defineConfig } from '@medusajs/framework/utils'
 loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
+  admin: {
+    backendUrl: process.env.MEDUSA_BACKEND_URL || 'http://localhost:9000',
+    disable: process.env.DISABLE_MEDUSA_ADMIN === 'true',
+  },
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    workerMode: process.env.MEDUSA_WORKER_MODE as 'shared' | 'worker' | 'server' || 'shared',
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      jwtSecret: process.env.JWT_SECRET || 'supersecret',
+      cookieSecret: process.env.COOKIE_SECRET || 'supersecret',
+    },
+  },
 })
