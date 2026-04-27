89415dde89850bd737135a53a60d968378c5fd5a
Joshjs1990 <joshstewart90@gmail.com>
2026-04-27 11:01:59 +0200

    Fix S3 provider path

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -42,7 +42,7 @@ module.exports = defineConfig({
       options: {
         providers: [
           {
-            resolve: "@medusajs/file-s3",
+            resolve: "@medusajs/medusa/file-s3",
             id: "s3",
             options: {
               file_url: process.env.S3_FILE_URL,
